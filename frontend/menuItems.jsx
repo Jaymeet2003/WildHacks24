0e9ca91bef923c9cfd7cc0d9b6e09b4bc4462cb5
Jimmy <jaymeet5087d@gmail.com>
2024-04-06 23:34:03 -0500

    Sync

---
+++ frontend/menuItems.jsx
@@ -0,0 +1,53 @@
+import {dashboard, expenses, transactions, trend} from './Icons'
+
+export const menuItems = [
+    {
+        id: 1,
+        title: 'Dashboard',
+        icon: dashboard,
+        link: '/dashboard'
+    },
+    {
+        id: 2,
+        title: "View Transactions",
+        icon: transactions,
+        link: "/dashboard",
+    },
+    {
+        id: 3,
+        title: "Incomes",
+        icon: trend,
+        link: "/dashboard",
+    },
+    {
+        id: 4,
+        title: "Expenses",
+        icon: expenses,
+        link: "/dashboard",
+    },
+    {   
+        id: 5,
+        title: "Savings Battles",
+        icon: dashboard,
+        link: "/",
+    },
+    {
+        id: 6,
+        title: "Earn Rewards",
+        icon: dashboard,
+        link: "/",
+    },
+    {
+        id: 7,
+        title: "Retirement Calculator",
+        icon: dashboard,
+        link: "/",
+    },
+    {
+        id: 8,
+        title: "Portfolio Tracker",
+        icon: dashboard,
+        link: "/",
+    },
+    
+]
