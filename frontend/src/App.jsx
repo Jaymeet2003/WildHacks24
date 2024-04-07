b6e56e51b41751464b5721c4cf9f92b7ba8854e2
Jimmy <jaymeet5087d@gmail.com>
2024-04-07 01:15:30 -0500

    updated avatar and name to get it from google

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
+// import "./App.css";
 import Dashboard from "./dashboard.jsx";
 import LoginPage from "./Loginpage.jsx";
 import Dash from "./Dash.jsx";
