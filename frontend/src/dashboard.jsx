544b4af51238bc0c3a3722a2e3454b57acad2d00
jashp007 <104731665+jashp007@users.noreply.github.com>
2024-04-07 10:26:56 -0500

    budgeting tip updated

--- frontend/src/dashboard.jsx
+++ frontend/src/dashboard.jsx
@@ -56,9 +56,9 @@ function Dashboard() {
                             </div>
                         </div>
                     </div>
-                    {/* <div className="tip">
+                    <div className="tip">
                         <h2><Tip /></h2>
-                    </ div>   */}
+                    </ div>  
                 </div>
         </DashboardStyled>
     )
